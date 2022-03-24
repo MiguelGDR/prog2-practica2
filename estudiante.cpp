c3b22bcfbea5275f4c28e8599ef515b762953802
Miguel <miguelgrvirtual@gmail.com>
2022-03-24 19:46:34 +0100

    Clase Estudiante Casi Implementada

--- estudiante.cpp
+++ estudiante.cpp
@@ -1,20 +1,30 @@
 #include "Estudiante.hpp"
 #include <cassert> 
+#include <iostream>
+
+using namespace std;
 
 namespace  bblProgIIB{
+// Contructores y Destructor
     // Constructor por defecto
-    Estudiante::Estudiante() : nombre( ), dni(0), edad(0), sexo( ), grado( )
-    {
-
+    Estudiante::Estudiante(){
     } // Fin del constructor por defecto
 
     // Constructor 
-    Estudiante::Estudiante(const std::string &nNombre, const std::string &nDni, int nEdad, char nSexo, std::string nGrado, const TDeportes &nDeportes)
-    : nombre(nNombre), dni(nDni), edad(nEdad), sexo(nSexo), grado(nGrado), deportes(nDeportes)
+    Estudiante::Estudiante(const std::string &nNombre, const std::string &nDni, int nEdad, char nSexo, std::string nGrado, const TDeportes &nDeportes) : nombre(nNombre), dni(nDni), edad(nEdad), sexo(nSexo), grado(nGrado), deportes(nDeportes)
     {
         assert(nEdad>0);
     } // Fin del constructor
 
+    // Constructor copia
+    Estudiante::Estudiante(const Estudiante &otro) : nombre(otro.nombre), dni(otro.dni), edad(otro.edad), sexo(otro.sexo), grado(otro.grado), deportes(otro.deportes)
+    {
+    }// Fin del constructor copia
+
+    // Destructor
+    Estudiante::~Estudiante(){
+    }
+// Fin constructores y destructor
 
     // Implementación de los métodos
     void Estudiante::asignarDNI(const std::string &d){
@@ -39,6 +49,31 @@ namespace  bblProgIIB{
 
     void Estudiante::asignarDeportes(const TDeportes &dep){
         deportes = dep;
+    }
+
+// Métodos get() para acceder a los atributos privados
+    std::string Estudiante::obtenerDni() const{
+        return dni;
+    }
+
+    std::string Estudiante::obtenerNombre() const{
+        return nombre;
+    }  
+
+    int Estudiante::obtenerEdad() const{
+        return edad;
+    }            
+    
+    char Estudiante::obtenerSexo() const{
+        return sexo;
+    }
+    
+    std::string Estudiante::obtenerGrado() const{
+        return grado;
+    }
+    
+    TDeportes Estudiante::obtenerDeportes() const{
+        return deportes;
     }
 
 } //Fin namespace bblProgIIB
